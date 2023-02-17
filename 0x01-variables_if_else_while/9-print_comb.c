7bf73370a52a6b4811eb9b1424e9655dde0ab611
bluecom3t <mdaniels26@protonmail.com>
2023-02-17 11:29:40 -0800

    Commit update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Print all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(',');
+		}
+		c++;
+	}
+}
