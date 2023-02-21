764cac5fb04f69981a653de97dd5abf0aaac28bc
bluecom3t <mdaniels26@protonmail.com>
2023-02-21 09:28:31 -0800

    Commit Update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,7 @@ void print_to_98(int n)
 			if (i != 98)
 				printf("%d, ",  i);
 			else if (i == 98)
-				printf("%d\n", i);
+				printf("%d\n",  i);
 		}
 	}
 	else if (n >= 98)
@@ -28,7 +28,7 @@ void print_to_98(int n)
 			if (j != 98)
 				printf("%d,",  j);
 			else if (j == 98)
-				printf("%d\n", j);
+				printf("%d\n",  j);
 		}
 	}
 }
