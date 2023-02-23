65916fdec4e4c7e449deb0fe0cc6aed6eefe6481
bluecom3t <mdaniels26@protonmail.com>
2023-02-23 12:44:11 -0800

    Commit update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,7 +8,7 @@ void print_triangle(int size)
 {
 	if (size <= 0)
 	{
-		_putchar("\n");
+		_putchar('\n');
 	}
 	else
 	{
@@ -25,7 +25,7 @@ void print_triangle(int size)
 				_putchar('#');
 			}
 
-			_putchar("\n");
+			_putchar('\n');
 		}
 	}
 }
