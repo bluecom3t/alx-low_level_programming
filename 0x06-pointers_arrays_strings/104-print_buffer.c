033de7152c0477b7db8ccd6438792390d1a8ef41
bluecom3t <mdaniels26@protonmail.com>
2023-03-02 11:44:18 -0800

    Update

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -19,11 +19,11 @@ void print_buffer(char *b, int size)
 		printf("\n");
 		return;
 	}
-	while (o < size )
+	while (o < size)
 	{
-		j = size - o < 10 ? size - o: 10;
+		j = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
-		for (i = 0; i < 10; i ++)
+		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
 				printf("%02x", *(b + o + i));
